6ff049ec5797e3fd15241af1e460e824664f1b82
tactica24 <okwafa@gmail.com>
2026-02-14 13:25:48 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,20 @@
-import path from "path";
 import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
-  outputFileTracingRoot: __dirname,
   images: {
     formats: ["image/avif", "image/webp"]
+  },
+  outputFileTracingRoot: resolve(__dirname),
+  webpack: (config, { dev }) => {
+    if (!dev) config.cache = false;
+    return config;
   },
   headers: async () => {
     return [
